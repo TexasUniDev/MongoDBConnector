13bcb430fe466208ed0a35ddbe724e7e052543e8
valentimarco <gamespy67@gmail.com>
2025-02-03 00:32:23 +0100

    Init project

---
+++ src/client/kotlin/com/mongodbconnector/MongoDBConnectorClient.kt
@@ -0,0 +1,9 @@
+package com.mongodbconnector
+
+import net.fabricmc.api.ClientModInitializer
+
+object MongoDBConnectorClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
